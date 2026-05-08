c4fe926903c9d479def9df0de769154919f87099
Umuthan Özel <ozelumuthan@gmail.com>
2026-05-08 23:35:48 +0300

    Add input management & camera movement

---
+++ Game/Assets/Scripts/CaveGame/Game/CameraManager.cs
@@ -0,0 +1,72 @@
+using System;
+using CaveGame.Services;
+using UnityEngine;
+
+namespace CaveGame.Game
+{
+    public class CameraManager : MonoBehaviour
+    {
+        [Header("Movement")]
+        [SerializeField] private float _moveSpeed = 100;
+        [SerializeField] private float _smoothTime = .15f;
+        private Vector2 _moveInput;
+        private Vector3 _currentVelocity;
+
+        [Header("Zoom")]
+        [SerializeField] private float _zoomSpeed = 1.5f;
+        [SerializeField] private float _minZoom = 3f;
+        [SerializeField] private float _maxZoom = 15f;
+        private float _zoomInput;
+        private float _targetZoom;
+
+        private Camera _camera;
+
+        private void Start()
+        {
+            _camera = GetComponent<Camera>();
+
+            _targetZoom = _camera.orthographicSize;
+
+            var inputService = ServiceLocator.Get<InputService>();
+            inputService.CameraMoveInputChanged += OnCameraMoveInputChanged;
+            inputService.CameraZoomInputChanged += OnCameraZoomInputChanged;
+        }
+
+        private void OnDestroy()
+        {
+            var inputService = ServiceLocator.Get<InputService>();
+            inputService.CameraMoveInputChanged -= OnCameraMoveInputChanged;
+            inputService.CameraZoomInputChanged -= OnCameraZoomInputChanged;
+        }
+
+        private void OnCameraZoomInputChanged(float input) => _zoomInput = input;
+        private void OnCameraMoveInputChanged(Vector2 input) => _moveInput = input;
+
+        private void LateUpdate()
+        {
+            MoveCamera();
+            ZoomCamera();
+        }
+
+        private void MoveCamera()
+        {
+            Vector3 inputDirection = (transform.right * _moveInput.x) + (transform.forward * _moveInput.y);
+            Vector3 normalizedInputDirection = new Vector3(inputDirection.x, 0, inputDirection.z).normalized;
+
+            Vector3 targetPosition = transform.position + (normalizedInputDirection * _moveSpeed * Time.deltaTime);
+
+            transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref _currentVelocity, _smoothTime);
+        }
+
+        private void ZoomCamera()
+        {
+            if (Mathf.Abs(_zoomInput) > float.Epsilon)
+            {
+                _targetZoom -= _zoomInput * _zoomSpeed;
+                _targetZoom = Mathf.Clamp(_targetZoom, _minZoom, _maxZoom);
+            }
+
+            _camera.orthographicSize = Mathf.Lerp(_camera.orthographicSize, _targetZoom, Time.deltaTime * 10f);
+        }
+    }
+}
---
+++ Game/Assets/Scripts/CaveGame/Services/InputService.cs
@@ -0,0 +1,44 @@
+using System;
+using CaveGame.Input;
+using UnityEngine;
+
+namespace CaveGame.Services
+{
+    public class InputService : MonoBehaviour, IService
+    {
+        private PlayerControlsInput _controls;
+
+        public event Action<Vector2> CameraMoveInputChanged;
+        public event Action<float> CameraZoomInputChanged;
+
+        private void Awake()
+        {
+            ServiceLocator.Register<InputService>(this);
+            DontDestroyOnLoad(gameObject);
+
+            _controls = new PlayerControlsInput();
+
+            //  _controls.Gameplay.MoveCamera.performed += ctx => CameraMoveInputChanged?.Invoke(ctx.ReadValue<Vector2>());
+            _controls.Gameplay.MoveCamera.performed += ctx => CameraMoveInputChanged?.Invoke(ctx.ReadValue<Vector2>());
+            _controls.Gameplay.MoveCamera.canceled += ctx => CameraMoveInputChanged?.Invoke(Vector2.zero);
+
+            _controls.Gameplay.ZoomCamera.performed += ctx =>
+            {
+                float rawScroll = ctx.ReadValue<float>();
+                float normalizedScroll = Mathf.Sign(rawScroll) * (Mathf.Abs(rawScroll) > 0 ? 1 : 0);
+                CameraZoomInputChanged?.Invoke(normalizedScroll);
+            };
+            _controls.Gameplay.ZoomCamera.canceled += ctx => CameraZoomInputChanged?.Invoke(0f);
+        }
+
+        private void OnEnable()
+        {
+            _controls.Gameplay.Enable();
+        }
+
+        private void OnDisable()
+        {
+            _controls.Gameplay.Disable();
+        }
+    }
+}
